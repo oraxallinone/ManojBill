29e8fa84c6c262f044bd2dda4c468f3aa4d4ad09
sudama <oraxallinone@gmail.com>
2026-01-17 13:20:26 +0530

    some text

---
+++ HotelBillQueries.sql
@@ -0,0 +1,16 @@
+--use cbtsplco_annapurna
+
+select * from InvoiceDetail
+INotes IDONumber
+
+select * from ItemTransaction
+ItemmPart
+
+
+select * from [dbo].[ItemMaster]
+where ItemDetails like '%paper%'
+
+
+
+select * from [dbo].[ItemMaster]
+where Rate like '%500%'
